af0b8ebed2c28ddc3822b223f839e197de612fd9
prajapati26 <prajapatisachin83387@example.com>
2025-07-01 19:41:42 +0530

    newwww

--- obj/Debug/net9.0/FormIOProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/FormIOProject.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormIOProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62c04f8cd495f00f8f9737e4b8f0e78af449669a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormIOProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormIOProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
